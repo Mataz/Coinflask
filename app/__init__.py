c5968e0f3c967d83128622b8787cdd6ff2296169
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-03-01 18:53:24 +0100

    Update __init__.py
    Added current_year() and path to the static folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
+app.__static_folder = 'STATIC_FOLDER_PATH'
 
-from app import routes
+from app import general
+
+@app.context_processor
+def current_year():
+    return {'current_year': datetime.utcnow().year}
