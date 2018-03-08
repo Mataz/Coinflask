079a7d904941f3f30c9470ff2f60ab4bfafccda9
Thomas Mahieux <thomas.mahieux@gmail.com>
2018-03-08 16:47:19 +0100

    Update general.py
    Deleted btc_chg() and added a new function regrouping the changes of BTC, ETH and NANO

--- app/general.py
+++ app/general.py
@@ -106,36 +106,107 @@ def get_top5_volume():
     return html_data
 
 
-def btc_chg():
+def thirty_days_chg():
     btc_url = 'http://coincap.io/history/30day/BTC'
     btc_data = requests.get(btc_url).json()
-    price_list = []
-    time_list = []
-    df_price = {}
+    nano_url = 'http://coincap.io/history/30day/NANO'
+    nano_data = requests.get(nano_url).json()
+    eth_url = 'http://coincap.io/history/30day/ETH'
+    eth_data = requests.get(eth_url).json()
+
     img = io.BytesIO()
 
+    btc_price_list = []
+    btc_time_list = []
+    btc_df_price = {}
     for values in btc_data['price']:
         time = values[0]
         time = int(str(time)[:10])
-        converted_time = datetime.datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
+        converted_time = datetime.datetime.utcfromtimestamp(time).strftime(
+            '%Y-%m-%d %H:%M:%S')
 
-        df_price.setdefault('Datetime', [])
-        df_price['Datetime'].append(converted_time)
-        df_price.setdefault('Price', [])
-        df_price['Price'].append(str(values[1]))
+        btc_df_price.setdefault('Datetime', [])
+        btc_df_price['Datetime'].append(converted_time)
+        btc_df_price.setdefault('Price', [])
+        btc_df_price['Price'].append(str(values[1]))
 
-        df = pd.DataFrame(df_price, columns=['Datetime', 'Price'])
+        btc_df = pd.DataFrame(btc_df_price, columns=['Datetime', 'Price'])
 
-        price_list.append(str(values[1]))
-        time_list.append(converted_time)
+        btc_price_list.append(str(values[1]))
+        btc_time_list.append(converted_time)
 
-    df.to_csv('btc_30d_chg.csv')
-    graph = pd.read_csv('btc_30d_chg.csv', parse_dates=True, index_col=1)
-    graph['Price'].plot()
+    btc_df.to_csv('btc_30d_chg.csv')
+
+    nano_price_list = []
+    nano_time_list = []
+    nano_df_price = {}
+    for values in nano_data['price']:
+        time = values[0]
+        time = int(str(time)[:10])
+        converted_time = datetime.datetime.utcfromtimestamp(time).strftime(
+            '%Y-%m-%d %H:%M:%S')
+
+        nano_df_price.setdefault('Datetime', [])
+        nano_df_price['Datetime'].append(converted_time)
+        nano_df_price.setdefault('Price', [])
+        nano_df_price['Price'].append(str(values[1]))
+
+        nano_df = pd.DataFrame(nano_df_price, columns=['Datetime', 'Price'])
+
+        nano_price_list.append(str(values[1]))
+        nano_time_list.append(converted_time)
+
+    nano_df.to_csv('nano_30d_chg.csv')
+
+    eth_price_list = []
+    eth_time_list = []
+    eth_df_price = {}
+
+    for values in eth_data['price']:
+        time = values[0]
+        time = int(str(time)[:10])
+        converted_time = datetime.datetime.utcfromtimestamp(time).strftime(
+            '%Y-%m-%d %H:%M:%S')
+
+        eth_df_price.setdefault('Datetime', [])
+        eth_df_price['Datetime'].append(converted_time)
+        eth_df_price.setdefault('Price', [])
+        eth_df_price['Price'].append(str(values[1]))
+
+        eth_df = pd.DataFrame(eth_df_price, columns=['Datetime', 'Price'])
+
+        eth_price_list.append(str(values[1]))
+        eth_time_list.append(converted_time)
+
+    eth_df.to_csv('eth_30d_chg.csv')
+
+    btc_graph = pd.read_csv('btc_30d_chg.csv', parse_dates=True, index_col=1)
+    nano_graph = pd.read_csv('nano_30d_chg.csv', parse_dates=True, index_col=1)
+    eth_graph = pd.read_csv('eth_30d_chg.csv', parse_dates=True, index_col=1)
+
+    x = btc_graph['Price']
+    y = eth_graph['Price']
+    z = nano_graph['Price']
+
+    f, axarr = plt.subplots(3, sharex=True, figsize=(8, 12))
+    # f.suptitle('Bitcoin and NANO changes (30d)')
     plt.xlabel('Date')
     plt.ylabel('Price')
-    plt.title('Bitcoin changes (30d)')
-    plt.savefig(img, format='png')
+    axarr[0].plot(x)
+    axarr[0].set_title('Bitcoin')
+    axarr[1].plot(y)
+    axarr[1].set_title('Ethereum')
+    axarr[2].plot(z)
+    axarr[2].set_title('NANO')
+
+    f.set_facecolor('#F2F2F2')
+
+    plt.gca().set_aspect('equal', adjustable='datalim')
+
+    f.subplots_adjust(hspace=0.3)
+
+    # plt.show()
+    plt.savefig(img, format='png', facecolor=f.get_facecolor())
     img.seek(0)
 
     plot_url = base64.b64encode(img.getvalue()).decode()
@@ -148,9 +219,9 @@ def index():
     top5_percent = get_top5_inc()
     top5_mktcap = get_top5_mktcap()
     top5_volume = get_top5_volume()
-    btc_graph = btc_chg()
+    graphs = thirty_days_chg()
     return render_template('index.html', tables=[top5_percent, top5_mktcap, top5_volume],
-                           graph=btc_graph,
+                           graphs=graphs,
                            titles=['coins', 'Top 5 by percent increase in the last 24 hours.',
                                    'Top 5 by Market Capitalization.', 'Top 5 by 24h Trade Volume.'])
 
